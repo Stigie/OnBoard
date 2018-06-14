066a342a8bd8641beeffb1c2c53ca02e20807371
Stigie <stiggie@info.sgu.ru>
2018-06-15 00:14:54 +0400

    add change username on submit with mobx

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import styles from "./styles.module.css";
+import InputTextStore from '../../store/index';
 
 class Form extends Component{
-
     render(){
       return(
-        <form className={styles.main}>
+        <form className={styles.main} onSubmit={(e) => { InputTextStore.clickOnsubmit(e) }}>
           <input 
+            id="username"
             className={styles.input} 
             placeholder="Username"  />
           <button className={styles.button}>Submit</button>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,14 @@
 import logo from '../../assets/logo.svg';
 import React, { Component } from 'react';
 import styles from "./styles.module.css";
+import InputTextStore from '../../store/index';
+import { observer } from 'mobx-react';
 
-class Header extends Component{
-    render(){
-      let msg = this.props.inputText;
-      if (msg ===""){
-        msg = "username"
-      }
-      return(
+const Header = observer(() => (
         <header className={styles.cover}>
           <img src = {logo}/>
-        <h1 className={styles.h1}>Welcome on board, &#123;username&#125;!</h1>
+        <h1 className={styles.h1}>Welcome on board, &#123;{InputTextStore.getInputText}&#125;!</h1>
         </header>
-      );
-    }
-  }
-
+       ) 
+  )
   export default Header;
